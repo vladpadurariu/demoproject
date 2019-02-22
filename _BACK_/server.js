abd3773312e41ba25599b47fb8dc60974ae2c1d5
vladpadurariu <vladjunglemassive@gmail.com>
2019-02-22 11:11:06 +0100

    first commit

---
+++ _BACK_/server.js
@@ -0,0 +1,11 @@
+const X = require('express');
+const APP = X();
+const PORT = 7777;
+
+
+APP.use('/', X.static(__dirname + '/_SITE_/'));
+
+
+APP.listen(PORT, () => {
+    console.log(`\r\nNODE ::: I started my back end server on port ${PORT}.\r\n`);
+});
